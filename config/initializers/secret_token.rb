4dc2406b4c882e5be203e180d90e437763c9ea51
Cyle Hunter <cylehunter33@gmail.com>
2013-04-19 14:01:48 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TournamentSandbox::Application.config.secret_token = '3dd8e0a56851a22b7dc1721f2d79108ac9fac5e2843bcf6f06ed90306da82dc58260d6269420a98b1db241ce59fce660e2688d0ebd4cd6e460d89d7764a4a806'
